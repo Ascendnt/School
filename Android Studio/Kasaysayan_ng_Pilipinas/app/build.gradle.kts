0ff7bcfbe55bbf2941504fa01d610c744e953849
Kenneth Balantucas <kennethbalantucas14@gmail.com>
2023-11-03 23:29:29 +0800

    app v1.1

--- Android Studio/Kasaysayan_ng_Pilipinas/app/build.gradle.kts
+++ Android Studio/Kasaysayan_ng_Pilipinas/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.kasaysayan_ng_pilipinas"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.kasaysayan_ng_pilipinas"
