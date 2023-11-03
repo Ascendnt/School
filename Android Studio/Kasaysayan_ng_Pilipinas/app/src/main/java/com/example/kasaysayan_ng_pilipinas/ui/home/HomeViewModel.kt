b63da95a1c15553032d1c9397842cf9f932516b3
Kenneth Balantucas <kennethbalantucas14@gmail.com>
2023-11-04 00:09:55 +0800

    app still v1 not v1.1

--- Android Studio/Kasaysayan_ng_Pilipinas/app/src/main/java/com/example/kasaysayan_ng_pilipinas/ui/home/HomeViewModel.kt
+++ Android Studio/Kasaysayan_ng_Pilipinas/app/src/main/java/com/example/kasaysayan_ng_pilipinas/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "First Section"
     }
     val text: LiveData<String> = _text
 }
